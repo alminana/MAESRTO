241bec372354e4bfae6d10033113e4a83d4e8ce4
alminana <alminanaeilerbutad@yahoo.com>
2021-04-01 01:00:14 +0800

    Updates

---
+++ precache-manifest.399797f3b46f5453ce04130d6e3917dc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632cde3029808c56eb625ae516464f29",
+    "url": "/MAESRTO/index.html"
+  },
+  {
+    "revision": "9be308d6b532ced0d8a4",
+    "url": "/MAESRTO/static/css/main.cf91dff5.chunk.css"
+  },
+  {
+    "revision": "efd92a0424321fb7edcb",
+    "url": "/MAESRTO/static/js/2.0205606e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MAESRTO/static/js/2.0205606e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be308d6b532ced0d8a4",
+    "url": "/MAESRTO/static/js/main.fb9981a3.chunk.js"
+  },
+  {
+    "revision": "072b2d4a2be0962d601a",
+    "url": "/MAESRTO/static/js/runtime-main.e5e3146e.js"
+  },
+  {
+    "revision": "84bc78e2cd3f9011d6c3054facc1e2fe",
+    "url": "/MAESRTO/static/media/bg.84bc78e2.jpg"
+  },
+  {
+    "revision": "337c866f059cdfcc1368dba54d45f9a4",
+    "url": "/MAESRTO/static/media/pizza.337c866f.jpg"
+  },
+  {
+    "revision": "d101237b5188b4fbd20a4a9b6824062a",
+    "url": "/MAESRTO/static/media/pizza1.d101237b.png"
+  },
+  {
+    "revision": "ace97dc2b3f5e0d95f51a7da16c48d83",
+    "url": "/MAESRTO/static/media/pizza2.ace97dc2.png"
+  },
+  {
+    "revision": "f9d26f1702947d07bb07e54967c55fd6",
+    "url": "/MAESRTO/static/media/pizza3.f9d26f17.png"
+  },
+  {
+    "revision": "2096a09a0055909f9cd782b69f359e21",
+    "url": "/MAESRTO/static/media/pizza4.2096a09a.png"
+  },
+  {
+    "revision": "ba6c807bd8af1327b0d56ce603f2baf8",
+    "url": "/MAESRTO/static/media/pizza5.ba6c807b.png"
+  },
+  {
+    "revision": "6c87c0ad713bf58542c0b4ec394d0124",
+    "url": "/MAESRTO/static/media/pizza6.6c87c0ad.png"
+  }
+]);
